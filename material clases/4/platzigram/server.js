8d40c2a98ed9752f91317cfd3e6443cc44040115
Danny <dessignio@gmail.com>
2017-04-22 10:11:08 -0500

    server.js

---
+++ material clases/4/platzigram/server.js
@@ -0,0 +1,19 @@
+// variable express es igual a la libreria instalada por npm en node modules
+var express = require('express');
+
+// var app (puede ser cualquier nombre) es igual a express convertida en function
+var app = express();
+
+// app get ejecuta la ruta req = require Y res = response se usa res.send para enviar un mensaje
+app.get('/', function(req, res) {
+    res.send('Hola Mundo');
+})
+
+// app listen tiene por parametro el puerto (3000) luego la function pasa parametro err
+app.listen(3000, function(err) {
+    if (err) return console.log('hubo un error'), process.exit(1);
+
+    console.log('escuchando en el puerto 3000');
+})
+
+// uso node serve.js en consola para verificar el funcionamiento del servidor
