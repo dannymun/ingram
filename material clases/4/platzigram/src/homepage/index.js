396918272abd32d0156c5694548629cfb20aa861
Danny <dessignio@gmail.com>
2017-05-08 14:10:18 -0500

    funcion like final y fecha automatizada

--- material clases/4/platzigram/src/homepage/index.js
+++ material clases/4/platzigram/src/homepage/index.js
@@ -3,30 +3,31 @@ var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
 
-page('/', function (ctx, next) {
-  title('Platzigram');
-  var main = document.getElementById('main-container');
+page('/', function(ctx, next) {
+    title('Platzigram');
+    var main = document.getElementById('main-container');
 
-  var pictures = [
-    {
-      user: {
-        username: 'slifszyc',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
-      },
-      url: 'office.jpg',
-      likes: 10,
-      liked: true
-    },
-    {
-      user: {
-        username: 'slifszyc',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
-      },
-      url: 'office.jpg',
-      likes: 2,
-      liked: true
-    }
-  ];
+    var pictures = [{
+            user: {
+                username: 'Danny',
+                avatar: 'https://fb-s-c-a.akamaihd.net/h-ak-xtl1/v/t1.0-9/16807097_405384326476179_258405160304733262_n.jpg?oh=e892b2440b21fa630cb030836b6e2317&oe=59866172&__gda__=1501310787_1d58f047d528192befebbfcf259f9c0b'
+            },
+            url: 'office.jpg',
+            likes: 10,
+            liked: false,
+            createdAt: new Date()
+        },
+        {
+            user: {
+                username: 'Danna',
+                avatar: 'https://fb-s-c-a.akamaihd.net/h-ak-xtl1/v/t1.0-9/16807097_405384326476179_258405160304733262_n.jpg?oh=e892b2440b21fa630cb030836b6e2317&oe=59866172&__gda__=1501310787_1d58f047d528192befebbfcf259f9c0b'
+            },
+            url: 'office.jpg',
+            likes: 2,
+            liked: true,
+            createdAt: new Date().setDate(new Date().getDate() - 10)
+        }
+    ];
 
-  empty(main).appendChild(template(pictures));
+    empty(main).appendChild(template(pictures));
 })
