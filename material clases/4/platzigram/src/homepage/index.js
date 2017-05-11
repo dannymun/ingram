9742850daad5ac7058b0659607556533a2ec9d1f
Danny <dessignio@gmail.com>
2017-05-11 15:01:03 -0500

    formatJS cambiar idioma de los post

--- material clases/4/platzigram/src/homepage/index.js
+++ material clases/4/platzigram/src/homepage/index.js
@@ -13,7 +13,7 @@ page('/', function(ctx, next) {
                 avatar: 'https://fb-s-c-a.akamaihd.net/h-ak-xtl1/v/t1.0-9/16807097_405384326476179_258405160304733262_n.jpg?oh=e892b2440b21fa630cb030836b6e2317&oe=59866172&__gda__=1501310787_1d58f047d528192befebbfcf259f9c0b'
             },
             url: 'office.jpg',
-            likes: 10,
+            likes: 0,
             liked: false,
             createdAt: new Date()
         },
@@ -23,7 +23,7 @@ page('/', function(ctx, next) {
                 avatar: 'https://fb-s-c-a.akamaihd.net/h-ak-xtl1/v/t1.0-9/16807097_405384326476179_258405160304733262_n.jpg?oh=e892b2440b21fa630cb030836b6e2317&oe=59866172&__gda__=1501310787_1d58f047d528192befebbfcf259f9c0b'
             },
             url: 'office.jpg',
-            likes: 2,
+            likes: 1,
             liked: true,
             createdAt: new Date().setDate(new Date().getDate() - 10)
         }
