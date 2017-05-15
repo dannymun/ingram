01e79d0e5d815c83db753f108a5ec402b8384e5e
Danny <dessignio@gmail.com>
2017-05-15 12:49:02 -0500

    servidores

--- material clases/4/platzigram/server.js
+++ material clases/4/platzigram/server.js
@@ -6,49 +6,48 @@ app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Platzigram' });
+app.get('/', function(req, res) {
+    res.render('index', { title: 'Platzigram' });
 })
 
-app.get('/signup', function (req, res) {
-  res.render('index', { title: 'Platzigram - Signup' });
+app.get('/signup', function(req, res) {
+    res.render('index', { title: 'Platzigram - Signup' });
 })
 
-app.get('/signin', function (req, res) {
-  res.render('index', { title: 'Platzigram - Signin' });
+app.get('/signin', function(req, res) {
+    res.render('index', { title: 'Platzigram - Signin' });
 })
 
-app.get('/api/pictures', function (req, res, next) {
-  var pictures = [
-    {
-      user: {
-        username: 'slifszyc',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
-      },
-      url: 'office.jpg',
-      likes: 0,
-      liked: false,
-      createdAt: new Date().getTime()
-    },
-    {
-      user: {
-        username: 'slifszyc',
-        avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
-      },
-      url: 'office.jpg',
-      likes: 1,
-      liked: true,
-      createdAt: new Date().setDate(new Date().getDate() - 10)
-    }
-  ];
+app.get('/api/pictures', function(req, res, next) {
+    var pictures = [{
+            user: {
+                username: 'Danny',
+                avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+            },
+            url: 'office.jpg',
+            likes: 0,
+            liked: false,
+            createdAt: new Date().getTime()
+        },
+        {
+            user: {
+                username: 'Danny',
+                avatar: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/11031148_10153448564292612_2579019413701631604_n.jpg?oh=d83cdd0687c87c91b247a42375fc5a57&oe=57B12767'
+            },
+            url: 'office.jpg',
+            likes: 1,
+            liked: true,
+            createdAt: new Date().setDate(new Date().getDate() - 10)
+        }
+    ];
 
-  setTimeout(function () {
-    res.send(pictures);  
-  }, 2000)
+    setTimeout(function() {
+        res.send(pictures);
+    }, 2000)
 })
 
-app.listen(3000, function (err) {
-  if (err) return console.log('Hubo un error'), process.exit(1);
+app.listen(3000, function(err) {
+    if (err) return console.log('Hubo un error'), process.exit(1);
 
-  console.log('Platzigram escuchando en el puerto 3000');
+    console.log('Platzigram escuchando en el puerto 3000');
 })
--- material clases/4/platzigram/src/homepage/index.js
+++ material clases/4/platzigram/src/homepage/index.js
@@ -6,55 +6,55 @@ var request = require('superagent');
 var header = require('../header');
 var axios = require('axios');
 
-page('/', header, asyncLoad, function (ctx, next) {
-  title('Platzigram');
-  var main = document.getElementById('main-container');
+page('/', header, asyncLoad, function(ctx, next) {
+    title('Platzigram');
+    var main = document.getElementById('main-container');
 
-  empty(main).appendChild(template(ctx.pictures));
+    empty(main).appendChild(template(ctx.pictures));
 })
 
 function loadPictures(ctx, next) {
-  request
-    .get('/api/pictures')
-    .end(function (err, res) {
-      if (err) return console.log(err);
+    request
+        .get('/api/pictures')
+        .end(function(err, res) {
+            if (err) return console.log(err);
 
-      ctx.pictures = res.body;
-      next();
-    })
+            ctx.pictures = res.body;
+            next();
+        })
 }
 
 function loadPicturesAxios(ctx, next) {
-  axios
-    .get('/api/pictures')
-    .then(function (res) {
-      ctx.pictures = res.data;
-      next();
-    })
-    .catch(function (err) {
-      console.log(err);
-    })
+    axios
+        .get('/api/pictures')
+        .then(function(res) {
+            ctx.pictures = res.data;
+            next();
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
 }
 
 function loadPicturesFetch(ctx, next) {
-  fetch('/api/pictures')
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (pictures) {
-      ctx.pictures = pictures;
-      next();
-    })
-    .catch(function (err) {
-      console.log(err);
-    })
+    fetch('/api/pictures')
+        .then(function(res) {
+            return res.json();
+        })
+        .then(function(pictures) {
+            ctx.pictures = pictures;
+            next();
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
 }
 
 async function asyncLoad(ctx, next) {
-  try {
-    ctx.pictures = await fetch('/api/pictures').then(res => res.json());
-    next();
-  } catch (err) {
-    return console.log(err);
-  }
+    try {
+        ctx.pictures = await fetch('/api/pictures').then(res => res.json());
+        next();
+    } catch (err) {
+        return console.log(err);
+    }
 }
